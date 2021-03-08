e96b5e9b3506c1b26216eaf2815ca960e2863446
Redempt <instantiable@gmail.com>
2021-03-08 15:10:26 -0500

    Move tile drawing to Screen, add click to draw

--- LevelEditor/screen.lua
+++ LevelEditor/screen.lua
@@ -1,7 +1,9 @@
 require("tiles")
+require("camera")
 
 Screen = {}
 local tiles = {}
+local width, height = love.graphics.getDimensions()
 
 -- Initialize the screen
 function Screen.init(x, y)
@@ -16,13 +18,31 @@ function Screen.init(x, y)
     end
 end
 
+-- Check if a tile is within the bounds of the screen
+function Screen.isInBounds(x, y)
+    if x < 1 or y < 1 then
+        return false
+    end
+    return not (x > Screen.width or y > Screen.height)
+end
+
 -- Get the tile position from a pixel position
+function Screen.getTilePosition(x, y)
+    x = x - Camera.x
+    y = (y - Camera.y)
+    return math.floor(x / TileSize) + 1, Screen.height - math.floor(y / TileSize)
+end
+
+-- Get the screen pixel position from a tile position
 function Screen.getScreenPosition(x, y)
-    return x / TileSize, y / TileSize
+    return (x * TileSize) + Camera.x, (height - ((y + 1) * TileSize)) + Camera.y
 end
 
 -- Set a tile on the screen
 function Screen.setTile(x, y, tile)
+    if not Screen.isInBounds(x, y) then
+        return
+    end
     tiles[x][y] = tile
 end
 
@@ -39,7 +59,15 @@ end
 function Screen.drawScreen()
     for px = 1, Screen.width, 1 do
         for py = 1, Screen.height, 1 do
-            Tile.drawTile(Screen.getTile(px, py), px - 1, py - 1)
+            Screen.drawTile(Screen.getTile(px, py), px - 1, py - 1)
         end
     end
+end
+
+-- Draw a single tile to the screen. Inverts y so that 0 is the bottom of the window.
+function Screen.drawTile(tile, x, y)
+	love.graphics.setColor(tile.color[1] / 255, tile.color[2] / 255, tile.color[3] / 255, 1)
+	-- Account for camera position
+	x, y = Screen.getScreenPosition(x, y)
+	love.graphics.rectangle("fill", x, y, TileSize, TileSize)
 end
--- LevelEditor/tiles.lua
+++ LevelEditor/tiles.lua
@@ -14,8 +14,6 @@ Tile = {
 		color = {255, 0, 0}
 	}
 }
-
-local width, height = love.graphics.getDimensions()
 TileSize = 20
 
 -- Create a map of all of the tiles by name
@@ -25,12 +23,3 @@ for key, value in pairs(Tile) do
 	byName[value.name] = value
 end
 Tile.byName = byName
-
--- Draw a single tile to the screen. Inverts y so that 0 is the bottom of the window.
-function Tile.drawTile(tile, x, y)
-	love.graphics.setColor(tile.color[1] / 255, tile.color[2] / 255, tile.color[3] / 255, 1)
-	-- Account for camera position
-	local xPos = (x * TileSize) + Camera.x
-	local yPos = (height - ((y + 1) * TileSize)) + Camera.y
-	love.graphics.rectangle("fill", xPos, yPos, TileSize, TileSize)
-end
