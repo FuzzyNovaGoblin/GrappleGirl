155df6a36be3eb05d4677630456f009dacdd809d
Liam Cannon <liamcannon@Liams-MacBook-Pro-2.local>
2021-03-23 19:11:22 -0400

    Updated audio.play overload

--- GrappleGirlGame/Music.lua
+++ GrappleGirlGame/Music.lua
@@ -3,10 +3,10 @@
 
     -- overwrite love.audio.play to create and register source if needed
 local play = love.audio.play
-function love.audio.play(what, how, loop)
-    local src = what
-    if type(what) ~= "userdata" or not what:typeOf("Source") then
-        src = love.audio.newSource(what, how)
+function love.audio.play(audio, audio_type, loop)
+    local src = audio
+    if type(audio) ~= "userdata" or not audio:typeOf("Source") then
+        src = love.audio.newSource(audio, audio_type)
         src:setLooping(loop or false)
     end
 
@@ -19,5 +19,4 @@ local stop = love.audio.stop
 function love.audio.stop(src)
     if not src then return end
     stop(src)
-    sources[src] = nil
 end
