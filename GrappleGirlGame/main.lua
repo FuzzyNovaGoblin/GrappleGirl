996d7d68d58a9417e40a10ef535293a1ae7fbed7
Liam Cannon <liamcannon@Liams-MacBook-Pro-2.local>
2021-03-30 20:20:13 -0400

    Voice lines

--- GrappleGirlGame/main.lua
+++ GrappleGirlGame/main.lua
@@ -4,7 +4,7 @@ require("Music")
 
 function love.load()
     gGirl = Character:new(nil);
-    music = love.audio.play("Dioma.mp3", "stream", true)
+    music = love.audio.play("audio/music/Dioma.mp3", "stream", true)
     music:setVolume(0.75)
 end
 
@@ -14,8 +14,10 @@ end
 
 function love.keypressed(key)
     if key == 'p' then
+        -- plays from stopped position
         love.audio.play(music)
     elseif key == 's' then
+        -- only pauses audio doesn't reset
         love.audio.stop(music)
     end
 end
